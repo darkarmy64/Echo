bb567d93ed18c155ec3000da6c5741d50f44a150
shubhsin <luk4shubhs@gmail.com>
2015-10-28 21:44:54 +0530

    Initial Commit

---
+++ DUCEPRO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DUCEPRO
+//
+//  Created by Shubham Sorte on 28/10/15.
+//  Copyright © 2015 appvaders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DUCEPRO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DUCEPRO
+//
+//  Created by Shubham Sorte on 28/10/15.
+//  Copyright © 2015 appvaders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
